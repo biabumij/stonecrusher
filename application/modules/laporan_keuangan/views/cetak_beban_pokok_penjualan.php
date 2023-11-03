cedd4ff1ed68c92a9c5e527088794112b0e1532e
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-11-03 10:32:26 +0700

    update

--- application/modules/laporan_keuangan/views/cetak_beban_pokok_penjualan.php
+++ application/modules/laporan_keuangan/views/cetak_beban_pokok_penjualan.php
@@ -457,7 +457,10 @@
 				<th align="center">4.</th>
 				<th align="left">Harga Pokok Penjualan (Siap Jual)</th>
 				<th align="right"><?php echo number_format($stock_opname_bahan_jadi_bulan_lalu['volume'] + $total_rekapitulasi_produksi_harian,2,',','.');?> (Ton)</th>
-				<th align="right"><?php echo number_format(($stock_opname_bahan_jadi_bulan_lalu['volume'] * $harga_satuan_bahan_jadi_bulan_lalu['harga_satuan_bahan_jadi']) + $total_nilai_produksi_boulder + $total_biaya_peralatan + $total_nilai_produksi_solar + $total_operasional,0,',','.');?></th>
+				<?php
+				$nilai_hpp_siap_jual = ($stock_opname_bahan_jadi_bulan_lalu['volume'] * $harga_satuan_bahan_jadi_bulan_lalu['harga_satuan_bahan_jadi']) + $total_nilai_produksi_boulder + $total_biaya_peralatan + $total_nilai_produksi_solar + $total_operasional;
+				?>
+				<th align="right"><?php echo number_format($nilai_hpp_siap_jual,0,',','.');?></th>
 				<?php
 				$harga_siap_jual = (($stock_opname_bahan_jadi_bulan_lalu['volume'] * $harga_satuan_bahan_jadi_bulan_lalu['harga_satuan_bahan_jadi']) + $total_nilai_produksi_boulder + $total_biaya_peralatan + $total_nilai_produksi_solar + $total_operasional) / ($stock_opname_bahan_jadi_bulan_lalu['volume'] + $total_rekapitulasi_produksi_harian);
 				$harga_siap_jual_new = round($harga_siap_jual,0);
@@ -468,14 +471,17 @@
 				<th align="center">5.</th>
 				<th align="left">Persediaan Akhir Bahan Jadi</th>
 				<th align="right"><?php echo number_format($stock_opname_bahan_jadi_bulan_akhir['volume'],2,',','.');?> (Ton)</th>
-				<th align="right"><?php echo number_format(round($stock_opname_bahan_jadi_bulan_akhir['volume'],2) * $harga_siap_jual,0,',','.');?></th>
+				<?php
+				$nilai_persediaan_akhir = round($stock_opname_bahan_jadi_bulan_akhir['volume'],2) * $harga_siap_jual;
+				?>
+				<th align="right"><?php echo number_format($nilai_persediaan_akhir,0,',','.');?></th>
 				<th align="right"></th>
 			</tr>
 			<tr style="font-weight:bold;">
 				<th align="center">6.</th>
 				<th align="left">Jumlah Harga Pokok Penjualan</th>
 				<th align="right"><?php echo number_format(($stock_opname_bahan_jadi_bulan_lalu['volume'] + $total_rekapitulasi_produksi_harian) - $stock_opname_bahan_jadi_bulan_akhir['volume'],2,',','.');?> (Ton)</th>
-				<th align="right"><?php echo number_format((($stock_opname_bahan_jadi_bulan_lalu['volume'] * $harga_satuan_bahan_jadi_bulan_lalu['harga_satuan_bahan_jadi']) + $total_nilai_produksi_boulder + $total_biaya_peralatan + $total_nilai_produksi_solar + $total_operasional) - (round($stock_opname_bahan_jadi_bulan_akhir['volume'],2) * $harga_siap_jual_new),0,',','.');?></th>
+				<th align="right"><?php echo number_format($nilai_hpp_siap_jual - $nilai_persediaan_akhir,0,',','.');?></th>
 				<th align="right"></th>
 			</tr>
 			<tr style="font-weight:bold;">
