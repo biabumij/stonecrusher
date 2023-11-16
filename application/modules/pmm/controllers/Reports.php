034b1de4daf6d4bf1c6285fbb0edfc3351137911
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2023-11-16 15:14:30 +0700

    update

--- application/modules/pmm/controllers/Reports.php
+++ application/modules/pmm/controllers/Reports.php
@@ -1486,7 +1486,7 @@ class Reports extends CI_Controller {
 
 			//Total
 			$total_nilai_masuk = $total_nilai_pembelian + $total_nilai_pembelian_solar;
-			$total_nilai_keluar = $total_nilai_produksi + $total_nilai_produksi_solar;
+			$total_nilai_keluar = $total_nilai_produksi + $total_nilai_produksi_loss + $total_nilai_produksi_solar;
 			$total_nilai_akhir = $total_nilai_produksi_loss_akhir + $total_nilai_produksi_akhir_solar;
 	        ?>
 			
