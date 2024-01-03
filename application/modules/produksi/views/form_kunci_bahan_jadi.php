7c81f1a68a331a2c147858a2975253704c7f81e3
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2024-01-03 15:21:12 +0700

    update

--- application/modules/produksi/views/form_kunci_bahan_jadi.php
+++ application/modules/produksi/views/form_kunci_bahan_jadi.php
@@ -72,7 +72,7 @@
                                     </div>
 									<br />
                                     <div class="row">
-                                        <div class="col-sm-12 text-right">
+                                        <div class="col-sm-12 text-center">
                                             <a href="<?= site_url('admin/kunci_&_approval');?>" class="btn btn-danger" style="margin-bottom:0; width:15%; font-weight:bold; border-radius:10px;"><i class="fa fa-close"></i> Batal</a>
                                             <button type="submit" class="btn btn-success" style="width:15%; font-weight:bold; border-radius:10px;"><i class="fa fa-send"></i> Kirim</button>
                                         </div>
