9e0955bf8e1cfd48a7ada2f06173071c4702dd6f
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2024-01-11 15:49:26 +0700

    update

--- application/modules/produksi/views/script_material_on_site.php
+++ application/modules/produksi/views/script_material_on_site.php
@@ -8,7 +8,8 @@
         showDropdowns: true,
         locale: {
             format: 'DD-MM-YYYY'
-        }
+        },
+        minDate: new Date(2023, 07, 01),
     });
     $('.dtpicker').on('apply.daterangepicker', function(ev, picker) {
         $(this).val(picker.startDate.format('DD-MM-YYYY'));
