2ce95e3fd7a89601068d3ecd729d98ead032afae
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2024-01-15 15:45:56 +0700

    update

--- application/modules/penjualan/controllers/Penjualan.php
+++ application/modules/penjualan/controllers/Penjualan.php
@@ -1712,7 +1712,7 @@ class Penjualan extends Secure_Controller
         $this->db->update("pmm_penawaran_penjualan");
 
         $this->db->update('pmm_penawaran_penjualan', array('status' => 'CLOSED'), array('id' => $id));
-        $this->session->set_flashdata('notif_success', 'Penawaran Closed');
+        $this->session->set_flashdata('notif_reject', '<b>CLOSED</b>');
         redirect("admin/penjualan");
     }
 
@@ -1808,7 +1808,7 @@ class Penjualan extends Secure_Controller
 		$this->db->set("updated_on", date('Y-m-d H:i:s'));
         $this->db->where("id", $id);
         $this->db->update("pmm_sales_po");
-        $this->session->set_flashdata('notif_success', 'Sales Order Closed');
+		$this->session->set_flashdata('notif_reject', '<b>CLOSED</b>');
         redirect("admin/penjualan");
     }
 
