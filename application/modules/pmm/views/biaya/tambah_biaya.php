36195e32a5de291317fb24c9aedab19246430faf
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2024-08-01 14:58:06 +0700

    update

--- application/modules/pmm/views/biaya/tambah_biaya.php
+++ application/modules/pmm/views/biaya/tambah_biaya.php
@@ -87,7 +87,7 @@
                                         <div class="col-sm-3">
                                             <label>Cara Pembayaran</label>
                                             <?php $pembayaran = ["Transfer","Cash","Cek&Giro"]; ?>
-                                            <select name="cara_pembayaran" class="form-control">
+                                            <select name="cara_pembayaran" class="form-control" required="">
                                                 <option value="">Pilih Cara Pembayaran</option>
                                                 <?php foreach($pembayaran as $p) : ?>
                                                 <option value="<?= $p; ?>"><?= $p; ?></option>
