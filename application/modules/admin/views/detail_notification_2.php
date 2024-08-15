74c8f77f32fb2ae1a59179e41c847660c3ff41da
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2024-08-15 18:37:04 +0700

    update

--- application/modules/admin/views/detail_notification_2.php
+++ application/modules/admin/views/detail_notification_2.php
@@ -28,7 +28,7 @@
                     <div class="col-sm-12 col-lg-12">
                         <div class="panel">
                             <div class="panel-header"> 
-                                <h3>Butuh Persetujuan Ka. Plant</h3>
+                                <h3>Butuh Persetujuan Ka. Unit Bisnia</h3>
                                 <div class="text-left">
                                 <a href="<?php echo site_url('admin');?>">
                                 <button style="color:white; background-color:#5bc0de; border:1px solid black; border-radius:10px; line-height:30px;"><b>KEMBALI KE DASHBOARD</b></button></a>
