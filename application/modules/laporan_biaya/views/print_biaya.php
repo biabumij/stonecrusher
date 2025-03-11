1de484bbb66acd65258df075f8a7eeeeab60faee
Bayu <ginanjar.bayubimantoro@biabumijayendra.com>
2025-03-11 11:33:42 +0700

    update

--- application/modules/laporan_biaya/views/print_biaya.php
+++ application/modules/laporan_biaya/views/print_biaya.php
@@ -196,67 +196,45 @@
 				<td width="20%" align="right"><b><?= $this->filter->Rupiah($total_c);?></b></td>
 			</tr>
 		</table>
-		<table width="98%" border="0" cellpadding="50">
+		<table width="98%" border="0" cellpadding="30">
 			<tr >
 				<td width="5%"></td>
 				<td width="90%">
-					<table width="100%" border="0" cellpadding="3">
+					<table width="100%" border="0" cellpadding="2">
 						<tr>
 							<td align="center" >
-								Disetujui Oleh
+								Dibuat Oleh
 							</td>
-							<td align="center" >
+							<td align="center">
 								Diperiksa Oleh
 							</td>
 							<td align="center">
-								Dibuat Oleh
+								Disetujui Oleh
 							</td>
 						</tr>
-						<?php
-							$create = $this->db->select('*')
-							->from('kunci_bahan_jadi')
-							->where("(date = '$end_date')")
-							->order_by('id','desc')->limit(1)
-							->get()->row_array();
-
-							$this->db->select('a.admin_name, g.admin_group_name, a.admin_ttd');
-							$this->db->join('tbl_admin_group g','a.admin_group_id = g.admin_group_id','left');
-							$this->db->where('a.admin_id',$create['unit_head']);
-							$unit_head = $this->db->get('tbl_admin a')->row_array();
-
-							$this->db->select('a.admin_name, g.admin_group_name, a.admin_ttd');
-							$this->db->join('tbl_admin_group g','a.admin_group_id = g.admin_group_id','left');
-							$this->db->where('a.admin_id',$create['keu_1']);
-							$keu_1 = $this->db->get('tbl_admin a')->row_array();
-
-							$this->db->select('a.admin_name, g.admin_group_name, a.admin_ttd');
-							$this->db->join('tbl_admin_group g','a.admin_group_id = g.admin_group_id','left');
-							$this->db->where('a.admin_id',$create['keu_2']);
-							$keu_2 = $this->db->get('tbl_admin a')->row_array();
-						?>
 						<tr class="">
-							<td align="center" height="70px">
-								<img src="<?= $unit_head['admin_ttd']?>" width="70px">
+							<td align="center" height="30px">
+								<img src="uploads/ttd_rifka.png" width="30px">
 							</td>
 							<td align="center">
-								<img src="<?= $keu_1['admin_ttd']?>" width="70px">
+								<img src="uploads/ttd_dian.png" width="30px">
 							</td>
 							<td align="center">
-								<img src="<?= $keu_2['admin_ttd']?>" width="70px">
+								<img src="uploads/ttd_satria.png" width="30px">
 							</td>
 						</tr>
 						<tr>
-							<td align="center">
-								<b><u><?=  $unit_head['admin_name'];?></u><br />
-								Kepala Unit Bisnis</b>
-							</td>
-							<td align="center">
-							<b><u><?=  $keu_1['admin_name'];?></u><br />
+						<td align="center">
+								<b><u>Rifka Dian Bethary </u><br />
 								Pj. Keuangan & SDM</b>
 							</td>
 							<td align="center">
-							<b><u><?=  $keu_2['admin_name'];?></u><br />
-								Staff Keuangan & SDM</b>
+								<b><u>Dian Melinda Sari </u><br />
+								Pj. Produksi</b>
+							</td>
+							<td align="center">
+								<b><u>Satria Widura Drana Wisesa </u><br />
+								Ka. Unit Bisnis</b>
 							</td>
 						</tr>
 					</table>
